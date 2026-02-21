4408b1df95ad35aa1b5375058dfeb015012e5288
isebmo <sebastien@mouret.pro>
2026-02-21 15:08:33 +0100

    feat: initial voxpopuli cask v1.0.0

---
+++ Casks/voxpopuli.rb
@@ -0,0 +1,19 @@
+cask "voxpopuli" do
+  version "1.0.0"
+  sha256 "REMPLACER"
+
+  url "https://dl.tfada.fr/voxpopuli/VoxPopuli-#{version}.dmg"
+  name "VoxPopuli"
+  desc "Native macOS real-time voice transcription using Apple Neural Engine"
+  homepage "https://tfada.fr"
+
+  depends_on macos: ">= :sonoma"
+
+  app "VoxPopuli.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.voxpopuli.app",
+    "~/Library/Preferences/com.voxpopuli.app.plist",
+    "~/Documents/VoxPopuli",
+  ]
+end
